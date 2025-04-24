109ca35c5e35be98264f3e8a87ef2aeba68947b8
cleiron <pinchonclaire@gmail.com>
2025-04-25 00:13:51 +0200

    Modifs dans plusieurs fichiers

--- exdoop.c
+++ exdoop.c
@@ -3,59 +3,59 @@
 
 /* Reformate moi cette func c'est dégeu ! */
 int Calcul( int nb, int nb1, char operateur)
-
 {
     if (operateur == '+')
-{
-    return nb + nb1;
-}
-
+    {
+        return nb + nb1;
+    }
     else if (operateur == '-')
-{
-    return nb - nb1;
-}
-
+    {
+        return nb - nb1;
+    }
     else if (operateur == '*')
-{
-    return nb * nb1;
-}
-
+    {
+        return nb * nb1;
+    }
     else if (operateur = '/')
-    if (nb1 != 0) 
-{
-    return nb / nb1;
-}
- 
+    {
+        if (nb1 != 0) 
+        {
+            return nb / nb1;
+        }
+    }
     else 
-{
-     write(1, "Error\n", 7);
-
-    return 0;
+    {
+        write(1, "Error\n", 7);
+        return 0;
+    }
 }
-}
-
 
 void ft_putstr(char *str)
 {
     while (*str)
-    write(1, str++, 1); /*Tu peux tab c'est plus propre !*/
+    {
+        write(1, str++, 1); 
+    }
 }
 
 int main ()
 
 {
-    int nb, nb1; /*Evite au possible d'assigné deux variables sur la même ligne*/
+    int nb; 
+    int nb1; 
+    int resultat;
     char operateur;
     char chaine1 [50];
 
-
     ft_putstr("Quel est ton calcul ?");
     write(1, "\n",1);
-
-    scanf ("%d %c %d", &nb, &operateur, &nb1);
-    int resultat = Calcul(nb, nb1, operateur); /*Pas besoin de recaster en int!*/
-
-    sprintf(chaine1,"Le resultat de %d %c %d est :%d\n", nb, operateur,nb1, resultat);
+    scanf ("%d %c %d", &nb, &operateur, &nb1) != 3)
+	{
+		ft_putstr("Invalid input format.\n");
+		return (1);;
+    resultat = Calcul(nb, nb1, operateur);
+    sprintf(chaine1,"Le resultat de %d %c %d est :%d\n", nb, operateur,
+        nb1, resultat);
     ft_putstr(chaine1);
    
     return 0;
---
+++ ft_recursive_factorial.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int ft_recursive_factorial(int nb)
+{
+    if (nb < 0)
+
+    return 0;
+}
