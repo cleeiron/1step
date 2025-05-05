3c29ab258206a1ee2873d34cafe8eabba2100891
cleiron <pinchonclaire@gmail.com>
2025-05-05 16:37:14 +0200

    presque

--- essaie.c
+++ essaie.c
@@ -1,27 +1,22 @@
 #include<stdio.h>
 
- int addition(int nb,int nb1)
+int soustraction (int nb, int nb1)
 
 {
-    if ( nb < 0 || nb1 < 0 );
-        return 0;
-    {   
-        int result = nb + nb1;
-            return result;
-    }   
-    
+    int result = nb - nb1;
+    return result;
 
- }
+}
 
- int main()
+int main ()
 
- {
+{
     int nb , nb1, result;
 
-    printf("Choisis deux chiffres\n");
-    scanf("%d %d", &nb, &nb1);
-    result = addition(nb, nb1);
-    printf("Le resultat est : %d\n", result);
+    printf("Choisis deux nombres\n");
+    scanf("%d %d",&nb,&nb1);
+    result = soustraction(nb,nb1);
+    printf("Le resultat est:%d\n", result);
     return 0;
 
- }
+}
